1da71a2b02f05b1b30b19fd4296e6bcccc8d65b2
Fedor <dev.f1t1l@gmail.com>
2020-10-08 09:03:40 +0300

    Update Program.cs

--- FileSystem_deleteFiles/FileSystem_deleteFiles/Program.cs
+++ FileSystem_deleteFiles/FileSystem_deleteFiles/Program.cs
@@ -12,70 +12,77 @@ namespace InputOutput
     {
         static void Main()
         {
-            var dir = @"C:\Test";
-            Directory.CreateDirectory(dir);
-            Console.WriteLine("СОЗДАЮ ПАПКУ ПО ПУТИ: {0}", dir);
-            var file = new FileInfo(@"C:\Test\Test.txt").Create();
-            file.Close(); //Закрыть поток, иначе удалить нельзя!
-
-            for (int i = 0; i < 3; i++)
+            try
             {
-                new FileInfo(dir + @"\TXT" + i + ".txt").Create().Close();
-                new FileInfo(dir + @"\NWM" + i + ".nwm").Create().Close();
-                new FileInfo(dir + @"\PPP" + i + ".ppp").Create().Close();
-            }
+                var dir = @"C:\Test";
+                Directory.CreateDirectory(dir);
+                Console.WriteLine("СОЗДАЮ ПАПКУ ПО ПУТИ: {0}", dir);
+                var file = new FileInfo(@"C:\Test\Test.txt").Create();
+                file.Close(); //Закрыть поток, иначе удалить нельзя!
 
-            //var searchPattern = "*.txt";
-            var filteredFiles = Directory.EnumerateFiles(dir)
-                .Where(f => string.Equals(Path.GetExtension(f), ".txt", StringComparison.OrdinalIgnoreCase) ||
-                            string.Equals(Path.GetExtension(f), ".ppp", StringComparison.OrdinalIgnoreCase));
+                for (int i = 0; i < 3; i++)
+                {
+                    new FileInfo(dir + @"\TXT" + i + ".txt").Create().Close();
+                    new FileInfo(dir + @"\NWM" + i + ".nwm").Create().Close();
+                    new FileInfo(dir + @"\PPP" + i + ".ppp").Create().Close();
+                }
 
-            string[] files = Directory.GetFiles(dir);
-            foreach (var item in files)
-            {
-                Console.WriteLine(item);
-            }
-            Console.WriteLine("Создано {0} файлов.", files.Length);
-            Console.ReadKey();
+                //var searchPattern = "*.txt";
+                var filteredFiles = Directory.EnumerateFiles(dir)
+                    .Where(f => string.Equals(Path.GetExtension(f), ".txt", StringComparison.OrdinalIgnoreCase) ||
+                                string.Equals(Path.GetExtension(f), ".ppp", StringComparison.OrdinalIgnoreCase));
 
-            foreach (var item in filteredFiles)
-            {
-                File.Delete(item);
-            }
-
-            Console.WriteLine(new string('=', 30));
-            filteredFiles = Directory.EnumerateFiles(dir)
-                .Where(f => string.Equals(Path.GetExtension(f), ".txt", StringComparison.OrdinalIgnoreCase) ||
-                            string.Equals(Path.GetExtension(f), ".nwm", StringComparison.OrdinalIgnoreCase));
-            // files = Directory.GetFiles(@"C:\Test", "*.*");
-            if (filteredFiles.Count() != 0)
-            {
-                foreach (var item in filteredFiles)
+                string[] files = Directory.GetFiles(dir);
+                foreach (var item in files)
                 {
                     Console.WriteLine(item);
                 }
+                Console.WriteLine("Создано {0} файлов.", files.Length);
+                Console.ReadKey();
+
+                foreach (var item in filteredFiles)
+                {
+                    File.Delete(item);
+                }
+
+                Console.WriteLine(new string('=', 30));
+                filteredFiles = Directory.EnumerateFiles(dir)
+                    .Where(f => string.Equals(Path.GetExtension(f), ".txt", StringComparison.OrdinalIgnoreCase) ||
+                                string.Equals(Path.GetExtension(f), ".nwm", StringComparison.OrdinalIgnoreCase));
+                // files = Directory.GetFiles(@"C:\Test", "*.*");
+                if (filteredFiles.Count() != 0)
+                {
+                    foreach (var item in filteredFiles)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                else { Console.WriteLine("ПУСТО!"); }
+
+                Console.WriteLine(new string('=', 30));
+
+                foreach (var f in GetFilesToProcess(dir, new[] { ".nwm", ".ppp", ".txt" }))
+                {
+                    Debug.WriteLine(f);
+                    Console.WriteLine(f);
+                }
+
+                IEnumerable<string> GetFilesToProcess(string path, IEnumerable<string> extensions)
+                {
+                    return Directory.GetFiles(path, "*.*")
+                        .Where(f => extensions.Contains(Path.GetExtension(f).ToLower()));
+                }
+                Console.WriteLine("УДАЛЕНИЕ ПАПКИ {0}", dir);
+                Console.ReadKey();
+
+                Directory.Delete(dir, true);
+                Console.ReadKey();
             }
-            else { Console.WriteLine("ПУСТО!"); }
-
-            Console.WriteLine(new string('=', 30));
-
-            foreach (var f in GetFilesToProcess(dir, new[] { ".nwm", ".ppp", ".txt" }))
-            {
-                Debug.WriteLine(f);
-                Console.WriteLine(f);
-            }
-
-            IEnumerable<string> GetFilesToProcess(string path, IEnumerable<string> extensions)
-            {
-                return Directory.GetFiles(path, "*.*")
-                    .Where(f => extensions.Contains(Path.GetExtension(f).ToLower()));
-            }
-            Console.WriteLine("УДАЛЕНИЕ ПАПКИ {0}", dir);
-            Console.ReadKey();
-
-            Directory.Delete(dir, true);
-            Console.ReadKey();
             //new DirectoryInfo(path).GetFiles().Where(Current => Regex.IsMatch(Current.Extension, "\\.(aspx|ascx)", RegexOptions.IgnoreCase)
+            catch (Exception e)
+            {
+                Console.WriteLine("АшЫпКэ: {0} , ты не админ?!", e.ToString());
+            }
         }
     }
 }
