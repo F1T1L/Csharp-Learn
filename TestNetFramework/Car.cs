d9a5b73f9f2eab50ed2c9286f80509f95c9db5d2
Fedor <32012911+F1T1L@users.noreply.github.com>
2024-08-06 12:58:05 +0300

    Delete TestNetFramework directory

--- TestNetFramework/Car.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestNetFramework
-{
-    public class Car
-    {
-        public string PetName { get; private set; }
-        public int Speed { get; private set; }
-        public string Color { get; private set; }        
-    }
-}
